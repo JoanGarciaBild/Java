9522d4826252e637a7359493b7cd16481a5dec9f
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-06-25 00:37:02 -0700

    if-then statement

--- JavaCourse2025/src/Hello.java
+++ JavaCourse2025/src/Hello.java
@@ -2,7 +2,16 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello Tim");
-        System.out.println("Hello World");
+
+        boolean isAlien = false;
+        if(isAlien == false){
+            System.out.println("It is not an alien!");
+        }
+
+        int topScore = 100;
+        if(topScore == 100){
+            System.out.println("You got the high score!");
+        }
 
     }
 }
