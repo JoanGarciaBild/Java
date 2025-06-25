80b92e6bc0efb4be439954abef58b5e1d29de120
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-06-25 00:41:02 -0700

    Logical Operators

--- JavaCourse2025/src/Hello.java
+++ JavaCourse2025/src/Hello.java
@@ -8,9 +8,14 @@ public class Hello {
             System.out.println("It is not an alien!");
         }
 
-        int topScore = 100;
-        if(topScore == 100){
+        int topScore = 80;
+        if(topScore <= 100){
             System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if((topScore > secondTopScore) || (topScore < 100)){
+            System.out.println("Greater than second top score and less than 100");
         }
 
     }
