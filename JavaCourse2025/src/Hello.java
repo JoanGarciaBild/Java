736829ee098a646de91e57d0ce78ed9eb9a3eb8b
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-06-25 00:54:49 -0700

    Challenge with everything of this cap

--- JavaCourse2025/src/Hello.java
+++ JavaCourse2025/src/Hello.java
@@ -1,31 +1,49 @@
 public class Hello {
     public static void main(String[] args) {
+//
+//        System.out.println("Hello Tim");
+//
+//        boolean isAlien = false;
+//        if(isAlien == false){
+//            System.out.println("It is not an alien!");
+//        }
+//
+//        int topScore = 80;
+//        if(topScore <= 100){
+//            System.out.println("You got the high score!");
+//        }
+//
+//        int secondTopScore = 60;
+//        if((topScore > secondTopScore) || (topScore < 100)){
+//            System.out.println("Greater than second top score and less than 100");
+//        }
+//
+//        int newValue = 50;
+//        if(newValue == 50){
+//            System.out.println("This isn't an error");
+//        }
+//
+//        boolean isCar = false;
+//        if(isCar != true){
+//            System.out.println("This is not supposed to happen");
+//        }
+//
+//        String makeOfCar = "Volkswagen";
+//        boolean isDomestic = makeOfCar == "Volkswagen" ? true : false;
+//
+//       if(isDomestic){
+//           System.out.println("This car is domestic to our country");
+//       }
 
-        System.out.println("Hello Tim");
+        double myFirstDouble = 20.00d;
+        double mySecondDouble = 80.00d;
+        double totalDouble = (myFirstDouble + mySecondDouble) * 100.00d;
+        double remaider = (totalDouble % 40.00d);
+        boolean myBoolean = remaider == 0.00d ? true : false;
+        System.out.println("Result = " + myBoolean);
 
-        boolean isAlien = false;
-        if(isAlien == false){
-            System.out.println("It is not an alien!");
-        }
-
-        int topScore = 80;
-        if(topScore <= 100){
-            System.out.println("You got the high score!");
-        }
-
-        int secondTopScore = 60;
-        if((topScore > secondTopScore) || (topScore < 100)){
-            System.out.println("Greater than second top score and less than 100");
-        }
-
-        int newValue = 50;
-        if(newValue == 50){
-            System.out.println("This isn't an error");
-        }
-
-        boolean isCar = false;
-        if(isCar != true){
-            System.out.println("This is not supposed to happen");
+        if(myBoolean != true){
+            System.out.println("Got some remainder");
         }
 
     }
