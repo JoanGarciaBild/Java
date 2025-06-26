64dccf134fbc5be7533e7e6f9c1867c3cc7f0ac7
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-06-25 17:14:17 -0700

    Return and parameters

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -2,29 +2,19 @@ public class MainChallenge {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
+        System.out.println("The final score is: " + calculateScore(true, 800, 5, 100));
 
-        boolean newGameOver = true;
-        int newScore = 10000;
-        int newLevelCompleted = 8;
-        int newBonus = 200;
-
-        int newFinalScore = newScore;
-
-        if(newGameOver){
-            newFinalScore += (newLevelCompleted * newBonus);
-            System.out.println("Your final score was: " + newFinalScore);
-        }
+        calculateScore(true, 10000, 8, 200);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         int finalScore = score;
 
         if(gameOver){
             finalScore += (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
         }
+        return finalScore;
     }
 
 }
