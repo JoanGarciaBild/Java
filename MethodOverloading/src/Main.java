113d8d1b6d1470f9fb5308e0b6d36377c1b59f1d
JoanGarciaBild <joangarciabild>
2025-07-03 11:43:36 -0700

    Practicing overload

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -1,9 +1,29 @@
 public class Main {
 
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("Unnamed played scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(){
+        System.out.println("No player name, no player score.");
+        return 0;
+    }
+    
+
     public static void main(String[] args) {
 
-
-
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore("100", 100);
+        calculateScore();
     }
 
 }
